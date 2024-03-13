3f066183e018202f72018d46f3257093a44c1d5a
Aymane Kssim <aymane.kssim@gmail.com>
2024-03-13 13:40:13 +0100

    minor fixes

--- developpement/MNIST/mnist_webapp.py
+++ developpement/MNIST/mnist_webapp.py
@@ -10,7 +10,6 @@ def recognize_digit(image):
     image = Image.fromarray(image.astype("uint8"))
     image = image.convert("1")
     image = image.resize((28, 28))
-    image.save("p.png")
     img_binary = io.BytesIO()
     image.save(img_binary, format="PNG")
     response = requests.post(
