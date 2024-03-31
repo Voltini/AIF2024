e1750eef03f72bc2bcb696c010ba8156797e81ef
Baptiste Giraud <bgiraud@cerfacs.fr>
2024-03-31 13:00:23 +0200

    Changes to ports, prep for text feature

--- developpement/project/helpers.py
+++ developpement/project/helpers.py
@@ -1,5 +1,8 @@
 import torchvision.transforms as transforms
 from torchvision import datasets
+from torch.utils.data import Dataset
+from transformers import DistilBertForSequenceClassification, DistilBertTokenizerFast
+import torch
 
 
 class ImageAndPathsDataset(datasets.ImageFolder):
@@ -12,6 +15,21 @@ class ImageAndPathsDataset(datasets.ImageFolder):
         return super().__len__()
 
 
+class NlpDataset(Dataset):
+    def __init__(self, data, labels, tokenizer):
+        self.data = data.to_list()
+        self.labels = labels.tolist()
+        self.encodings = tokenizer(self.data, truncation=True, padding=True)
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item["labels"] = torch.tensor(self.labels[idx], dtype=torch.long)
+        return item
+
+    def __len__(self):
+        return len(self.labels)
+
+
 def transform():
     mean = [0.485, 0.456, 0.406]
     std = [0.229, 0.224, 0.225]
@@ -20,6 +38,11 @@ def transform():
         [transforms.Resize((224, 224)), transforms.ToTensor(), normalize]
     )
     return transform
+
+
+def tokenizer():
+    tokenizer = DistilBertTokenizerFast.from_pretrained("distilbert-base-uncased")
+    return tokenizer
 
 
 def search(annoy_index, query_vector, k=5):
